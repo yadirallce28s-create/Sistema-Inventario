2779aae37456f87b0d9bf8977f913442f6753c50
Yadira Llactahuamani <yadirallce28s@gmail.com>
2026-06-19 10:26:45 -0500

    Crear Publicidad

---
+++ frontend/src/pages/Publicidad.jsx
@@ -0,0 +1,15 @@
+function Publicidad() {
+  return (
+    <div>
+      <h1>Publicidad</h1>
+
+      <ul>
+        <li>Campaña de Vacunación</li>
+        <li>Promoción de Baño y Corte</li>
+        <li>Descuento en Consultas</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Publicidad;
