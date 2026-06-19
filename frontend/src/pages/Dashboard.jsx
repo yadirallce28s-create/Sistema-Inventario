db80b5c56ddb1c9acab380a43e0786ed9a2700be
Yadira Llactahuamani <yadirallce28s@gmail.com>
2026-06-19 15:48:19 -0500

    Diseñar Dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,36 @@
 import MainLayout from "../layout/MainLayout";
-
+import "../css/dashboard.css";
+export default Dashboard;
 const Dashboard = () => {
   return (
-    <MainLayout>
+    <div>
       <h1>Dashboard DokyPets</h1>
-    </MainLayout>
+
+      <div className="cards">
+
+        <div className="card">
+          <h3>Mascotas</h3>
+          <p>120</p>
+        </div>
+
+        <div className="card">
+          <h3>Clientes</h3>
+          <p>85</p>
+        </div>
+
+        <div className="card">
+          <h3>Citas</h3>
+          <p>34</p>
+        </div>
+
+        <div className="card">
+          <h3>Productos</h3>
+          <p>56</p>
+        </div>
+
+      </div>
+
+    </div>
   );
 };
 
-export default Dashboard;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,29 +1,55 @@
 import "../css/login.css";
-
+import logo from "../assets/hero.png";
+import perrito from "../assets/perrito.png";
 const Login = () => {
   return (
     <div className="login-container">
-      <div className="login-card">
+
+      <div className="left-panel">
+
+        <img
+          src={logo}
+          alt="DokyPets"
+          className="logo"
+        />
+        <img
+          src={perrito}
+          alt="DokyPets"
+          className="logo"
+        />
 
         <h1>DokyPets</h1>
 
-        <h3>Inicio de Sesión</h3>
-
-        <input
-          type="text"
-          placeholder="Usuario"
-        />
-
-        <input
-          type="password"
-          placeholder="Contraseña"
-        />
-
-        <button>
-          Ingresar
-        </button>
+        <p>Sistema de Gestión Veterinaria</p>
 
       </div>
+
+      <div className="right-panel">
+
+        <div className="login-card">
+
+          <h2>Bienvenido</h2>
+
+          <p>Inicia sesión para continuar</p>
+
+          <input
+            type="text"
+            placeholder="Usuario"
+          />
+
+          <input
+            type="password"
+            placeholder="Contraseña"
+          />
+
+          <button>
+            Ingresar
+          </button>
+
+        </div>
+
+      </div>
+
     </div>
   );
 };
