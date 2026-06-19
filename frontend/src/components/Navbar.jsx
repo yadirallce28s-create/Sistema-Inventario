9ea008391f90069c6361d51c773308fecf9f6136
Yadira Llactahuamani <yadirallce28s@gmail.com>
2026-06-19 10:30:40 -0500

    Crear Navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#f8f9fa",
+        padding: "15px",
+        borderBottom: "1px solid #ddd"
+      }}
+    >
+      <h3>Panel de Control DokyPets</h3>
+    </div>
+  );
+}
+
+export default Navbar;
