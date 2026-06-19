b2ad847ebeeeaf3755eafc43a8e1522336c8f165
Yadira Llactahuamani <yadirallce28s@gmail.com>
2026-06-19 10:25:45 -0500

    Crear Servicios

---
+++ frontend/src/pages/Servicios.jsx
@@ -0,0 +1,24 @@
+function Servicios() {
+  return (
+    <div>
+      <h1>Servicios Veterinarios</h1>
+
+      <div>
+        <h3>Baño y Corte</h3>
+        <p>S/ 40</p>
+      </div>
+
+      <div>
+        <h3>Consulta General</h3>
+        <p>S/ 50</p>
+      </div>
+
+      <div>
+        <h3>Vacunación</h3>
+        <p>S/ 35</p>
+      </div>
+    </div>
+  );
+}
+
+export default Servicios;
