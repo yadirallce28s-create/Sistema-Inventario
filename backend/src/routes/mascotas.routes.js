108c97b3c73b3ade6662ef976608b8e4a72bd3df
“Gilary <221197@unamba.edu.pe>
2026-07-22 07:06:31 -0500

    actualización 5

--- backend/src/routes/mascotas.routes.js
+++ backend/src/routes/mascotas.routes.js
@@ -5,10 +5,16 @@ const router = express.Router();
 const {
   listarMascotas,
   registrarMascota,
+  editarMascota,
+  borrarMascota,
 } = require("../controllers/mascotas.controller");
 
 router.get("/", listarMascotas);
 
 router.post("/", registrarMascota);
+
+router.put("/:id", editarMascota);
+
+router.delete("/:id", borrarMascota);
 
 module.exports = router;
